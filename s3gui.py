bf16205854245cc50ea8021d5d92e166e7348fe6
Zeke <zekebarge@gmail.com>
2016-11-27 22:19:01 -0800

    App start files added

---
+++ s3gui.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 20:56:00 2016
+
+@author: Zeke
+"""
+
+import s3gui
+
+
+if __name__ == "__main__":
+    s3gui.main()
---
+++ s3gui.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+python ./s3gui.py
